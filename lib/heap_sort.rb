b2c98c0c37ae36027a1c0948dcbee7271b66330c
Elle Kosling <celloelle@gmail.com>
2019-09-21 15:08:36 -0700

    Finished add, remove, empty, and helper methods for min_heap.

--- lib/heap_sort.rb
+++ lib/heap_sort.rb
@@ -4,5 +4,7 @@
 # Time Complexity:  ?
 # Space Complexity: ?
 def heap_sort(list)
-  raise NotImplementedError, "Method not implemented yet..."
+  return list if list.length < 2
+
+  
 end
--- lib/min_heap.rb
+++ lib/min_heap.rb
@@ -17,7 +17,9 @@ class MinHeap
   # Time Complexity: ?
   # Space Complexity: ?
   def add(key, value = key)
-    raise NotImplementedError, "Method not implemented yet..."
+    @store << HeapNode.new(key, value)
+    index = @store.length-1
+    heap_up(index)
   end
 
   # This method removes and returns an element from the heap
@@ -25,7 +27,11 @@ class MinHeap
   # Time Complexity: ?
   # Space Complexity: ?
   def remove()
-    raise NotImplementedError, "Method not implemented yet..."
+    return nil if empty?
+    swap(0, @store.length-1)
+    removed_item = @store.pop
+    heap_down(0)
+    return removed_item.value
   end
 
 
@@ -47,7 +53,7 @@ class MinHeap
   # Time complexity: ?
   # Space complexity: ?
   def empty?
-    raise NotImplementedError, "Method not implemented yet..."
+    return @store.empty?
   end
 
   private
@@ -58,14 +64,32 @@ class MinHeap
   # Time complexity: ?
   # Space complexity: ?
   def heap_up(index)
-    
+    return if index == 0
+    parent_index = find_parent_index(index)
+    if @store[parent_index].key > @store[index].key
+      swap(parent_index, index)
+      heap_up(parent_index)
+    end
+    return
   end
 
   # This helper method takes an index and 
   #  moves it up the heap if it's smaller
   #  than it's parent node.
-  def heap_down(index)
-    raise NotImplementedError, "Method not implemented yet..."
+  def heap_down(parent_index)
+    left_child_index = find_left_child_index(parent_index)
+    right_child_index = find_right_child_index(parent_index)
+
+    return if !@store[left_child_index]
+
+    if !@store[right_child_index] || @store[left_child_index].key < @store[right_child_index].key
+      swap(left_child_index, parent_index)
+      heap_down(left_child_index)
+    else
+      puts "***** here ****"
+      swap(right_child_index, parent_index)
+      heap_down(right_child_index)
+    end
   end
 
   # If you want a swap method... you're welcome
@@ -73,5 +97,20 @@ class MinHeap
     temp = @store[index_1]
     @store[index_1] = @store[index_2]
     @store[index_2] = temp
+  end
+
+  # These helper methods find the index of the
+  # current item's parent node, and the indices
+  # of its left and right children
+  def find_parent_index(node_index)
+    return ((node_index-1)/2).floor
+  end
+
+  def find_left_child_index(node_index)
+    return node_index * 2 + 1
+  end
+
+  def find_right_child_index(node_index)
+    return node_index * 2 + 2
   end
 end
